59a8ca17b42e801489bedc78b7a2d69b6c1102b5
@omi-coder <omi-ufa@yandex.ru>
2023-01-07 17:08:26 +0300

    oauth added

--- todolist/settings.py
+++ todolist/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -24,8 +24,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'core',
     'social_django',
-    'corsheaders'
-
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
