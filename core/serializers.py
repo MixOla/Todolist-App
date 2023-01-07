a65cddbed9d5919180749d9d6eca5d5c716a5f82
@omi-coder <omi-ufa@yandex.ru>
2023-01-07 18:43:33 +0300

    oauth added

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import (
     authenticate,
-    get_user_model
+    get_user_model,
+    password_validation
 )
 from django.contrib.auth.hashers import make_password
 from rest_framework import (
@@ -16,10 +17,6 @@ class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     password_repeat = serializers.CharField(write_only=True)
 
-    class Meta:
-        model = USER_MODEL
-        fields = '__all__'
-
     def create(self, validated_data) -> USER_MODEL:
         password = validated_data.get('password')
         password_repeat = validated_data.pop('password_repeat')
@@ -27,21 +24,30 @@ class RegistrationSerializer(serializers.ModelSerializer):
         if password != password_repeat:
             raise serializers.ValidationError('Passwords don`t match')
 
+        try:
+            password_validation.validate_password(password)
+        except Exception as e:
+            raise serializers.ValidationError({'password': e.args[0]})
+
         hashed_password = make_password(password)
         validated_data['password'] = hashed_password
         instance = super().create(validated_data)
         return instance
 
+    class Meta:
+        model = USER_MODEL
+        fields = '__all__'
+
 
 class LoginSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True, write_only=True)
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(required=True)
 
     class Meta:
         model = USER_MODEL
         fields = ['username', 'password']
 
-    def create(self, validated_data) -> USER_MODEL:
+    def create(self, validated_data):
         user = authenticate(
             username=validated_data['username'],
             password=validated_data['password'],
--- todolist/urls.py
+++ todolist/urls.py
@@ -23,5 +23,6 @@ from django.urls import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/core/', include('core.urls')),
-    path('api/oauth/', include('social_django.urls', namespace='social')),
+    path('core/', include('core.urls')),
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
